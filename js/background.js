b29db74f92339b1cf2711d819617157692243c27
seouna <117568974+seouna@users.noreply.github.com>
2022-11-09 15:12:40 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -6,7 +6,7 @@ const images = ["img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg"];
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
 
-const bgImage=`url("images/${chosenImage}")`;
+const bgImage=`url("./images/${chosenImage}")`;
 
 console.log(bgImage);
 
